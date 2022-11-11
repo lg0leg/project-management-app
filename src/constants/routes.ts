c9e717302b2c702d2278e0fa2f387896dd591df1
Rustam Kot <ruskir7@gmail.com>
2022-11-12 02:30:17 +0300

    fix: added routes constant

---
+++ src/constants/routes.ts
@@ -0,0 +1,9 @@
+export const enum RoutesPath {
+  WELCOME = '/welcome',
+  SIGNIN = '/login',
+  SIGNUP = '/registration',
+  PROFILE = '/profile',
+  BOARDS = '/',
+  BOARD = '/board/:id',
+  NOT_FOUND = '/404',
+}
