3d63da115693ede2195a2ea54480cc69af997467
lg0leg <lg.oleg1067@gmail.com>
2022-11-14 16:22:29 +0300

    feat: improve footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
-import rslogo from './../assets/svg/rs_school_js.svg';
+import rslogo from './../assets/svg/logoBlue.svg';
 import { VscGithubAlt } from 'react-icons/vsc';
 import { SlSocialGithub } from 'react-icons/sl';
+import { useAppSelector } from 'app/hooks';
 
 export default function Footer() {
-  const [lang, setLang] = useState('en');
+  const { lang } = useAppSelector((state) => state.langReducer);
 
   return (
-    <footer className="flex min-h-[80px] flex-col items-center justify-around gap-[40px] bg-slate-400 p-[20px] text-2xl text-amber-400 sm:flex-row sm:gap-[0px]">
-      <div className="transition duration-300 hover:scale-105">
+    <footer className="flex min-h-[80px] flex-col items-center justify-around gap-[40px] bg-slate-100 p-[20px] text-2xl text-blue-500 sm:flex-row sm:gap-[0px]">
+      <div className=" transition duration-300 hover:rotate-3">
         <a href="https://rs.school/react/" target="_blank" rel="noreferrer">
           <img className="w-[100px]" src={rslogo} alt="rs logo" />
         </a>
       </div>
       <ul className=" flex flex-col gap-[10px] text-center sm:flex-row sm:gap-[30px]">
-        <li className="flex items-center gap-[5px] transition duration-300 hover:text-amber-500">
+        <li className="flex items-center gap-[5px] transition duration-300 hover:text-amber-400">
           <SlSocialGithub />
           <a href="https://github.com/NikitaKakurin" target="_blank" rel="noreferrer">
             {lang == 'en' ? 'Nikita' : 'Никита'}
           </a>
         </li>
-        <li className="flex items-center gap-[5px] transition duration-300 hover:text-amber-500">
+        <li className="flex items-center gap-[5px] transition duration-300 hover:text-amber-400">
           <VscGithubAlt />
           <a href="https://github.com/bycolour2" target="_blank" rel="noreferrer">
             {lang == 'en' ? 'Rustam' : 'Рустам'}
           </a>
         </li>
-        <li className="flex items-center gap-[5px] transition duration-300 hover:text-amber-500">
+        <li className="flex items-center gap-[5px] transition duration-300 hover:text-amber-400">
           <VscGithubAlt />
           <a href="https://github.com/lg0leg" target="_blank" rel="noreferrer">
             {lang == 'en' ? 'Oleg' : 'Олег'}
