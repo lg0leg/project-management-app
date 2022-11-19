0d7f0bba928428b4641583ef97e2d0d7ae60a532
Rustam Kot <ruskir7@gmail.com>
2022-11-19 15:34:05 +0300

    fix: fixed svg icons

--- src/components/AddModalContent.tsx
+++ src/components/AddModalContent.tsx
@@ -2,6 +2,7 @@ import { useAppSelector } from 'app/hooks';
 import { LangKey } from 'constants/lang';
 import { FC, ReactNode } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { HiXMark } from 'react-icons/hi2';
 import { Button } from './Button';
 
 interface IAddModalContentProps {
@@ -32,29 +33,17 @@ export const AddModalContent: FC<IAddModalContentProps> = ({ type, onCancel }) =
 
   return (
     <>
-      {/* <!-- Main modal --> */}
       <div className="w-[400px] overflow-y-auto overflow-x-hidden p-4">
         <div className="relative h-full w-full max-w-md md:h-auto">
-          {/* <!-- Modal content --> */}
           <div className="relative rounded-lg bg-white shadow">
             <button
               type="button"
               className="absolute top-3 right-2.5 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900"
               onClick={() => onCancel()}
             >
-              <svg
-                aria-hidden="true"
-                className="h-5 w-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              <div className="h-5 w-5">
+                <HiXMark size={20} />
+              </div>
               <span className="sr-only">{lang === LangKey.EN ? 'Close' : 'Закрыть'}</span>
             </button>
             <div className="py-6 px-6 ">
--- src/components/DeleteConformation.tsx
+++ src/components/DeleteConformation.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import { useAppSelector } from 'app/hooks';
 import { LangKey } from 'constants/lang';
 import { Button } from './Button';
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
+import { HiXMark } from 'react-icons/hi2';
 
 interface IDeleteConformationProps {
   type: string;
@@ -20,37 +22,15 @@ export const DeleteConformation: FC<IDeleteConformationProps> = ({ type, onConfi
             onClick={onCancel}
             className="absolute top-3 right-2.5 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900"
           >
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            <div className="h-5 w-5">
+              <HiXMark size={20} />
+            </div>
             <span className="sr-only">{lang === LangKey.EN ? 'Close' : 'Закрыть'}</span>
           </button>
           <div className="p-6 text-center">
-            <svg
-              aria-hidden="true"
-              className="mx-auto mb-4 h-14 w-14 text-gray-400"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
+            <div className="mx-auto mb-4 h-14 w-14 text-gray-400">
+              <AiOutlineExclamationCircle size={56} />
+            </div>
             <h3 className="mb-5 text-lg font-normal text-gray-500">
               Are you sure you want to delete this {type}?
             </h3>
