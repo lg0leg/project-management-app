d847390e4856852991a394adaa12a7edd304c033
NikitaKakurin <neyasut87@rambler.ru>
2022-11-22 18:23:34 +0300

    feat: add pointReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -6,6 +6,7 @@ import boardReducer from './slices/boardSlice';
 import columnReducer from './slices/columnSlice';
 import taskReducer from './slices/taskSlice';
 import fileReducer from './slices/fileSlice';
+import pointReducer from './slices/pointSlice';
 
 const rootReducer = combineReducers({
   authReducer,
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   columnReducer,
   taskReducer,
   fileReducer,
+  pointReducer,
 });
 
 export const store = configureStore({
