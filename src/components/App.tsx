9b322eb9b66c259a2a363fdbf5414cfa3a1aafdf
NikitaKakurin <neyasut87@rambler.ru>
2022-11-23 20:11:43 +0300

    feat: add init socket io(with console.log)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Main } from '../components/Main';
 import Footer from './Footer';
 import Header from './Header';
+import io from 'socket.io-client';
+
+const socket = io('https://final-task-backend-production-27b0.up.railway.app');
 
 function App() {
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('socket is connected');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('socket is disconnected');
+    });
+
+    socket.on('boards', (data) => {
+      console.log('socket boards', data);
+    });
+    socket.on('columns', (data) => {
+      console.log('socket columns', data);
+    });
+    socket.on('tasks', (data) => {
+      console.log('socket tasks', data);
+    });
+    socket.on('users', (data) => {
+      console.log('socket users', data);
+    });
+    socket.on('files', (data) => {
+      console.log('socket files', data);
+    });
+    socket.on('points', (data) => {
+      console.log('socket points', data);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('boards');
+      socket.off('columns');
+      socket.off('tasks');
+      socket.off('users');
+      socket.off('files');
+      socket.off('points');
+    };
+  }, []);
   return (
     <>
       <Header />
