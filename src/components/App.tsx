6db4bbaf010d09e4cfad63c0c00cd7b74d1fce42
NikitaKakurin <neyasut87@rambler.ru>
2022-11-29 15:21:43 +0300

    feat: add socket cbShow add task

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,10 +3,56 @@ import { Main } from '../components/Main';
 import Footer from './Footer';
 import Header from './Header';
 import io from 'socket.io-client';
+import { useLocation } from 'react-router-dom';
+import { RoutesPath } from 'constants/routes';
+import { BASE_URL } from 'constants/baseUrl';
+import type { ITask, IUser, IBoard } from 'models/dbTypes';
+import { apiToken } from 'API/API';
 
-const socket = io('https://final-task-backend-production-27b0.up.railway.app');
+const socket = io(BASE_URL);
+
+interface ISocketResponse {
+  action: string;
+  guid: string;
+  ids: string[];
+  initUser: string;
+  notify: boolean;
+  users: string[];
+}
+interface IParamsIds {
+  ids: string[];
+}
+
+const checkPath = (path: string, cbShow: () => void, cbDoAndShow?: () => void) => {
+  if (
+    path === RoutesPath.BOARDS ||
+    path === RoutesPath.WELCOME ||
+    path === RoutesPath.SIGN_IN ||
+    path === RoutesPath.SIGN_UP ||
+    path === RoutesPath.PROFILE
+  ) {
+    console.log('show only message');
+  }
+};
+const cbShow = async (event: string, data: ISocketResponse) => {
+  const { action, ids, users, notify, guid, initUser } = data;
+  if (event === 'tasks') {
+    if (action === 'add') {
+      if (!ids || !ids.length) return;
+      const responseTask = await apiToken<ITask[]>(`/tasksSet`, {
+        params: { ids: JSON.stringify(ids) },
+      });
+      const responseUser = await apiToken<IUser>(`/users/${responseTask.data[0].userId}`);
+      const responseBoard = await apiToken<IBoard>(`/boards/${responseTask.data[0].boardId}`);
+      console.log(
+        `Вам была добавлена таска ${responseTask.data[0].title} юзером ${responseUser.data.login} в доске ${responseBoard.data.title}`
+      );
+    }
+  }
+};
 
 function App() {
+  const location = useLocation();
   useEffect(() => {
     socket.on('connect', () => {
       console.log('socket is connected');
@@ -17,22 +63,29 @@ function App() {
     });
 
     socket.on('boards', (data) => {
-      console.log('socket boards', data);
+      // console.log('location', location);
+      // console.log('socket boards', data);
     });
     socket.on('columns', (data) => {
-      console.log('socket columns', data);
+      // console.log('location', location);
+      // console.log('socket columns', data);
     });
-    socket.on('tasks', (data) => {
+    socket.on('tasks', (data: ISocketResponse) => {
+      console.log('location', location);
       console.log('socket tasks', data);
+      cbShow('tasks', data);
     });
     socket.on('users', (data) => {
-      console.log('socket users', data);
+      // console.log('location', location);
+      // console.log('socket users', data);
     });
     socket.on('files', (data) => {
-      console.log('socket files', data);
+      // console.log('location', location);
+      // console.log('socket files', data);
     });
     socket.on('points', (data) => {
-      console.log('socket points', data);
+      // console.log('location', location);
+      // console.log('socket points', data);
     });
 
     return () => {
