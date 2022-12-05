4ba3d33df5100d0b455939254fc432a96213bed9
NikitaKakurin <neyasut87@rambler.ru>
2022-12-05 13:19:09 +0300

    fix: delete comments add LangKey in Welcome Page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -19,10 +19,10 @@ import Screen2 from '../assets/screenshots/2board.png';
 import Screen3 from '../assets/screenshots/3cards.png';
 import Screen4 from '../assets/screenshots/4change.jpg';
 import Screen5 from '../assets/screenshots/5colors.jpg';
+import { LangKey } from 'constants/lang';
 
 export const Welcome: FC = () => {
   const { lang } = useAppSelector((state) => state.langReducer);
-  const [showVideo, setShowVideo] = useState(false);
 
   const h4Styles = 'text-lg font-bold text-blue-500 sm:text-xl lg:text-4xl';
 
@@ -34,15 +34,17 @@ export const Welcome: FC = () => {
       <div className="min-h-[calc(100vh-100px-80px)] bg-gradient-to-r from-gray-50 to-blue-100 pb-[30px]">
         <section className="flex flex-col items-center justify-center gap-[30px] px-[40px] pt-[100px] sm:flex-row">
           <h2 className="text-xl font-bold text-blue-500 sm:text-3xl lg:text-5xl">
-            {lang == 'en' ? 'Accelerate your work ' : 'Ускорь свою работу '}
+            {lang == LangKey.EN ? 'Accelerate your work ' : 'Ускорь свою работу '}
             <br />
-            {lang == 'en' ? 'with Rocket Task Sheduler!' : 'вместе с Rocket Task Sheduler!'}
+            {lang == LangKey.EN ? 'with Rocket Task Sheduler!' : 'вместе с Rocket Task Sheduler!'}
           </h2>
           <img className="h-[100px] sm:h-[200px] lg:h-[360px]" src={Rocket} alt="rocket" />
         </section>
 
         <section className="flex flex-col items-center justify-center gap-[30px] pt-[100px]">
-          <h4 className={h4Styles}>{lang == 'en' ? 'How does it work?' : 'Как это работает?'}</h4>
+          <h4 className={h4Styles}>
+            {lang == LangKey.EN ? 'How does it work?' : 'Как это работает?'}
+          </h4>
           <div className="w-[80%] sm:w-[50%]">
             <Carousel
               dynamicHeight={true}
@@ -55,7 +57,7 @@ export const Welcome: FC = () => {
               <div>
                 <img src={Screen1} className="rounded-xl " />
                 <p className={carouselDescriptionStyles}>
-                  {lang == 'en'
+                  {lang == LangKey.EN
                     ? 'Step 1. Create an account. Just a username and password, nothing else!'
                     : 'Шаг 1. Создай аккаунт. Только логин и пароль, ничего лишнего!'}
                 </p>
@@ -63,7 +65,7 @@ export const Welcome: FC = () => {
               <div>
                 <img src={Screen2} className="rounded-xl " />
                 <p className={carouselDescriptionStyles}>
-                  {lang == 'en'
+                  {lang == LangKey.EN
                     ? 'Step 2: Create your first board'
                     : 'Шаг 2. Создай свою первую доску'}
                 </p>
@@ -71,7 +73,7 @@ export const Welcome: FC = () => {
               <div>
                 <img src={Screen3} className="rounded-xl " />
                 <p className={carouselDescriptionStyles}>
-                  {lang == 'en'
+                  {lang == LangKey.EN
                     ? 'Step 3. Add as many cards as you want!'
                     : 'Шаг 3. Добавляй столько карточек, сколько захочешь!'}
                 </p>
@@ -79,7 +81,7 @@ export const Welcome: FC = () => {
               <div>
                 <img src={Screen4} className="rounded-xl " />
                 <p className={carouselDescriptionStyles}>
-                  {lang == 'en'
+                  {lang == LangKey.EN
                     ? 'Step 4. Tasks can be dragged and changed'
                     : 'Шаг 4. Задачи можно перетаскивать и изменять'}
                 </p>
@@ -87,7 +89,7 @@ export const Welcome: FC = () => {
               <div>
                 <img src={Screen5} className="rounded-xl " />
                 <p className={carouselDescriptionStyles}>
-                  {lang == 'en' ? 'Step 5. Brighten it up!' : 'Шаг 5. Прибавь яркости!'}
+                  {lang == LangKey.EN ? 'Step 5. Brighten it up!' : 'Шаг 5. Прибавь яркости!'}
                 </p>
               </div>
             </Carousel>
@@ -95,7 +97,9 @@ export const Welcome: FC = () => {
         </section>
 
         <section className="flex flex-col items-center justify-center px-[40px] pt-[100px]">
-          <h4 className={h4Styles}>{lang == 'en' ? 'How is it done?' : 'Как это сделано?'}</h4>
+          <h4 className={h4Styles}>
+            {lang == LangKey.EN ? 'How is it done?' : 'Как это сделано?'}
+          </h4>
           <div className="flex flex-wrap items-center justify-center gap-[10px] py-[10px] sm:gap-[30px] sm:py-[30px]">
             <HelloPangea size={60} color={'rgb(59, 130, 246, 1)'} />
             <SiTailwindcss size={50} color={'rgb(59, 130, 246, 1)'} />
@@ -109,7 +113,7 @@ export const Welcome: FC = () => {
 
         <section className="flex flex-col items-center justify-center gap-[30px] px-[30px] pt-[80px] sm:px-[50px] md:px-[100px] lg:px-[10vw]">
           <h4 className={h4Styles}>
-            {lang == 'en' ? "Who's on our team?" : 'Кто в нашей команде?'}
+            {lang == LangKey.EN ? "Who's on our team?" : 'Кто в нашей команде?'}
           </h4>
           <ol className=" lg:grid lg:grid-cols-[1fr_1fr_1fr]">
             <li className="relative mb-6 lg:mb-0">
@@ -123,13 +127,13 @@ export const Welcome: FC = () => {
               </div>
               <div className="mt-3 sm:pr-8">
                 <h3 className="text-lg font-semibold text-gray-900 ">
-                  {lang == 'en' ? 'Nikita' : 'Никита'}
+                  {lang == LangKey.EN ? 'Nikita' : 'Никита'}
                 </h3>
                 <p className="mb-2 block text-sm font-normal leading-none text-gray-400 ">
-                  {lang == 'en' ? 'Team Leader' : 'Тимлид'}
+                  {lang == LangKey.EN ? 'Team Leader' : 'Тимлид'}
                 </p>
                 <p className="text-base font-normal text-gray-500 ">
-                  {lang == 'en'
+                  {lang == LangKey.EN
                     ? 'Created the project, set up the server side and interaction with it, added forms and authorization, organized work of the team.'
                     : 'Создал проект, настроил серверную часть и взаимодействие с ней, добавил формы и авторизацию, организовал работу команды.'}
                 </p>
@@ -147,13 +151,13 @@ export const Welcome: FC = () => {
               </div>
               <div className="mt-3 sm:pr-8">
                 <h3 className="text-lg font-semibold text-gray-900 ">
-                  {lang == 'en' ? 'Rustam' : 'Рустам'}
+                  {lang == LangKey.EN ? 'Rustam' : 'Рустам'}
                 </h3>
                 <p className="mb-2 block text-sm font-normal leading-none text-gray-400 ">
-                  {lang == 'en' ? 'Developer' : 'Разработчик'}
+                  {lang == LangKey.EN ? 'Developer' : 'Разработчик'}
                 </p>
                 <p className="text-base font-normal text-gray-500 ">
-                  {lang == 'en'
+                  {lang == LangKey.EN
                     ? 'Everything about the kanban board is his work: dragging columns and cards, adding/deleting/editing them, modal windows. Added routing and a 404 page.'
                     : 'Все что связано с канбан-доской, это его работа: перетаскивание колонок и карточек, их добавление/удаление/редактирование, модальные окна. Добавил роутинг и страницу 404.'}
                 </p>
@@ -171,13 +175,13 @@ export const Welcome: FC = () => {
               </div>
               <div className="mt-3 sm:pr-8">
                 <h3 className="text-lg font-semibold text-gray-900 ">
-                  {lang == 'en' ? 'Oleg' : 'Олег'}
+                  {lang == LangKey.EN ? 'Oleg' : 'Олег'}
                 </h3>
                 <p className="mb-2 block text-sm font-normal leading-none text-gray-400 ">
-                  {lang == 'en' ? 'Developer' : 'Разработчик'}
+                  {lang == LangKey.EN ? 'Developer' : 'Разработчик'}
                 </p>
                 <p className="text-base font-normal text-gray-500 ">
-                  {lang == 'en'
+                  {lang == LangKey.EN
                     ? 'Made a welcome page, a list of boards and their addition, a footer with a header, and various small useful components for the whole team.'
                     : 'Сделал страницу приветствия, список досок и их добавление, подвал с шапкой, и разные мелкие полезные компоненты для всей команды.'}
                 </p>
@@ -188,12 +192,12 @@ export const Welcome: FC = () => {
 
         <section className="flex flex-col items-center justify-center gap-[10px] px-[25px] pt-[100px] sm:gap-[30px] sm:px-[50px]">
           <h4 className={h4Styles}>
-            {lang == 'en'
+            {lang == LangKey.EN
               ? 'How to learn how to create the same cool sites?'
               : 'Как мне научится делать такие же крутые приложения?'}
           </h4>
           <p className="text-md font-semibold text-gray-500 sm:text-lg lg:text-3xl">
-            {lang == 'en' ? 'On free courses from ' : 'На бесплатных курсах от '}
+            {lang == LangKey.EN ? 'On free courses from ' : 'На бесплатных курсах от '}
             <a
               className="font-bold"
               href="https://rs.school/index.html"
@@ -202,37 +206,9 @@ export const Welcome: FC = () => {
             >
               Rolling Scopes School
             </a>
-            {lang == 'en' ? ', of course!' : ', конечно же!'}
+            {lang == LangKey.EN ? ', of course!' : ', конечно же!'}
           </p>
         </section>
-
-        {/* <section className="flex flex-col items-center gap-[20px] px-[30px] pt-[100px] text-center">
-          <h4 className={h4Styles}>
-            {lang == 'en'
-              ? 'Want to learn more about the app? '
-              : 'Хочешь узнать больше о приложении? '}
-            <br />
-            {lang == 'en' ? 'Watch this video!' : 'Смотри видео!'}
-          </h4>
-          {showVideo == false ? (
-            <CiYoutube
-              className="h-[100px] w-[100px] cursor-pointer hover:opacity-90 sm:h-[150px] sm:w-[150px]"
-              color={'rgb(59, 130, 246, 1)'}
-              onClick={() => setShowVideo(true)}
-            />
-          ) : (
-            <div className="relative flex h-0 w-[80%] justify-center overflow-hidden pb-[45%] shadow-lg sm:w-[50%] sm:pb-[28%]">
-              <iframe
-                className="t-0 l-0 absolute h-full w-full rounded-lg"
-                loading="lazy"
-                src="https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
-          )}
-        </section> */}
       </div>
     </>
   );
