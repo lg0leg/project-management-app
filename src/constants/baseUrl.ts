34f96886ef727e0e61613503fbadc6493f61057e
lg0leg <lg.oleg1067@gmail.com>
2022-12-13 15:19:51 +0300

    feat: change backend link

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://final-task-backend-production-27b0.up.railway.app/';
+// export const BASE_URL = 'https://final-task-backend-production-27b0.up.railway.app/';
+export const BASE_URL = 'https://final-task-backend-production-ef3e.up.railway.app/';
